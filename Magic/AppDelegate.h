ac54ebc9f06cbe826cd590e1737359564b623ae7
mezhevikin <mezhevikin@gmail.com>
2013-06-21 21:04:22 +0400

    Initial Commit

---
+++ Magic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Magic
+//
+//  Created by Администратор on 6/21/13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
